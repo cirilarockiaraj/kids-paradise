08dde03ab6fb55de982bd7fbd7cf4289dc48f86e
CIRIL AROCKIARAJ S <CIRIL AROCKIARAJ S@LAPTOP-2L3B5K3T>
2024-12-02 20:47:19 +0530

    initial commit

---
+++ kids-paradise/src/main/java/com/app/kids_paradise/KidsParadiseApplication.java
@@ -0,0 +1,13 @@
+package com.app.kids_paradise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsParadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsParadiseApplication.class, args);
+	}
+
+}
---
+++ kids-paradise/src/test/java/com/app/kids_paradise/KidsParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kids_paradise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsParadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
