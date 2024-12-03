fa4fdb336b090af07a992ba7561f7182b2ce8f93
CIRIL AROCKIARAJ S <CIRIL AROCKIARAJ S@LAPTOP-2L3B5K3T>
2024-12-03 20:56:28 +0530

    UI Enhancement and pipeline changed

--- kids-paradise/src/main/java/com/app/kids_paradise/controller/HomeController.java
+++ kids-paradise/src/main/java/com/app/kids_paradise/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.app.kids_paradise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/kids-paradise")
 public class HomeController {
 	
-	@GetMapping({"/"})
+	@GetMapping({"/",""})
 	public String getHomePage() {
 		return "index";
 	}
